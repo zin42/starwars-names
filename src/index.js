fe66642d307105b448f4227ae6717aa00e87c71c
zin42 <javierharford@gmail.com>
2017-08-17 14:04:47 +0100

    chore(releasing): Add travis config. conventional commit and semantic-release
    Did a bunch of stuff getting this to work, made a new variable to be removed called javi in
    index.js, tested and commited this file. That is all.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const starWarsNames = require('./starwars-names.json');
 const uniqueRandomArray = require('unique-random-array');
-
+const javi = 'fun';
 
 module.exports = {
     all: starWarsNames,
